9bca406791c13adb80b5e3456a6d2fbf3342f0c4
zvorkapicos <zvorkapic.os@gmail.com>
2020-05-24 19:16:28 +0200

    initial

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
 
-  $(".dropdown-toggle__help").click(function(){
+  // Topbar help dropdown
+  $(".toggle-help").click(function(){
+    $(event.currentTarget).toggleClass("toggle-active");
     $(".header-help").slideToggle(150);
+
+  // This closes the customer services dropdown
+    $(".toggle-customer-services").removeClass("toggle-active");
     $(".header-customer-services").slideUp(150);
   });
 
-  $(".dropdown-toggle__customer-services").click(function(){
-    $(".header-customer-services").slideToggle(150);
+  // Topbar customer services dropdown
+  $(".toggle-customer-services").click(function(){
+    $(event.currentTarget).toggleClass("toggle-active");
+    $(".header-customer-services").slideToggle(150);;
+
+  // This closes the hel dropdown
+    $(".toggle-help").removeClass("toggle-active");
     $(".header-help").slideUp(150);
   });
-
 
 });
