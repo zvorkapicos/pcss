3d734d62a3ac871a1dcbc0118bb49ff59354ed32
zvorkapicos <zvorkapic.os@gmail.com>
2020-06-12 11:57:37 +0200

    Navigation refactoring, submenus, js

--- dist/scripts/main-menu.js
+++ dist/scripts/main-menu.js
@@ -8,7 +8,7 @@ let navPrimary = document.querySelector(".navigation-wrapper");
 let pageOverlay = document.querySelector(".navigation-overlay");
 
 
-
+// Tu otvaram navigaciju
 headerActions.addEventListener("click", function(event){
     console.log(event.target);
     if(event.target == btnNavigation){
@@ -19,11 +19,28 @@ headerActions.addEventListener("click", function(event){
 
 });
 
+// Ovdje vadim parent width i dodajem ju na podizbornike jer position fixed ne jebe parenta
+    let parent = document.querySelector(".parent");
+    let parentStyles = window.getComputedStyle(parent);
+    let parentWidth = parentStyles.getPropertyValue("width");
+    document.querySelector(".main-menu-submenu").style.width = parentWidth;
 
+
+// Tu zatvaram navigaciju i jebavam podizbornike
 mainMenu.addEventListener("click", function (event){
     let btnClose = document.querySelector(".close");
     if(event.target == btnClose){
         pageOverlay.classList.remove("overlay-active");
         navPrimary.classList.remove("navigation-active");
     }
+
+    
+    
+
+    let parentMenu = Array.from(document.getElementsByClassName("parent"));
+    console.log("tu sam");
+
+    
+
+
 });
