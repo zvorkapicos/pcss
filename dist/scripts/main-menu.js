c64b049aa9f9159dd2495497dd2da4043172fa66
zvorkapicos <zvorkapic.os@gmail.com>
2020-06-13 17:43:12 +0200

    Fixed css for offcanvas nav, offcanvas nav JS complete

--- dist/scripts/main-menu.js
+++ dist/scripts/main-menu.js
@@ -1,46 +1,52 @@
 let headerActions = document.querySelector(".header-main__items");
 let btnNavigation = document.querySelector(".header-main__navigation-toggle");
-let btnAccount = document.querySelector(".header-main__actions-account");
-let btnCart = document.querySelector(".header-main__actions-cart");
+/* let btnAccount = document.querySelector(".header-main__actions-account");
+let btnCart = document.querySelector(".header-main__actions-cart"); */
 
-let mainMenu = document.querySelector(".navigation-wrapper");
 let navPrimary = document.querySelector(".navigation-wrapper");
 let pageOverlay = document.querySelector(".navigation-overlay");
 
-
 // Tu otvaram navigaciju
-headerActions.addEventListener("click", function(event){
-    console.log(event.target);
-    if(event.target == btnNavigation){
-        console.log("Navigation")
-        pageOverlay.classList.add("overlay-active");
-        navPrimary.classList.add("navigation-active");
-    }
-
+headerActions.addEventListener("click", function (event) {
+  console.log(event.target);
+  if (event.target == btnNavigation) {
+    console.log("Navigation");
+    pageOverlay.classList.add("overlay-active");
+    navPrimary.classList.add("navigation-active");
+  }
 });
 
-// Ovdje vadim parent width i dodajem ju na podizbornike jer position fixed ne jebe parenta
-    let parent = document.querySelector(".parent");
-    let parentStyles = window.getComputedStyle(parent);
-    let parentWidth = parentStyles.getPropertyValue("width");
-    document.querySelector(".main-menu-submenu").style.width = parentWidth;
+// Ovdje vadim parent width i dodajem ju na podizbornike jer position fixed ne jebe parenta nego window
+let parent = document.querySelector(".parent");
+let parentStyles = window.getComputedStyle(parent);
+let parentWidth = parentStyles.getPropertyValue("width");
+let submenus = Array.from(document.querySelectorAll(".main-menu-submenu"));
+submenus.forEach((item) => {
+  item.style.width = parentWidth;
+});
 
-
-// Tu zatvaram navigaciju i jebavam podizbornike
-mainMenu.addEventListener("click", function (event){
-    let btnClose = document.querySelector(".close");
-    if(event.target == btnClose){
-        pageOverlay.classList.remove("overlay-active");
-        navPrimary.classList.remove("navigation-active");
-    }
-
-    
-    
-
-    let parentMenu = Array.from(document.getElementsByClassName("parent"));
-    console.log("tu sam");
-
-    
-
-
+// Tu se roštiljaju podizbornici i zatvara navigacija
+let parentMenus = Array.from(document.getElementsByClassName("parent"));
+let goBack = Array.from(document.getElementsByClassName("back"));
+navPrimary.addEventListener("click", function (event) {
+  if (event.target.className == "") {
+    let target = event.target.parentElement;
+    target.children[1].classList.add("main-menu-submenu__active");
+  } else if (event.target.className == "back") {
+    let openedSubmenus = Array.from(
+      document.getElementsByClassName("main-menu-submenu")
+    );
+    openedSubmenus.forEach((item) => {
+      item.classList.remove("main-menu-submenu__active");
+    });
+  } else if (event.target.className == "close") {
+    pageOverlay.classList.remove("overlay-active");
+    navPrimary.classList.remove("navigation-active");
+    let openedSubmenus = Array.from(
+      document.getElementsByClassName("main-menu-submenu")
+    );
+    openedSubmenus.forEach((item) => {
+      item.classList.remove("main-menu-submenu__active");
+    });
+  }
 });
